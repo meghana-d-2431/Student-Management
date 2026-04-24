d024a9ac12f104438014b2e358fadb687d54545d
Megh <meghana@caredocs.ai>
2026-04-23 19:45:29 -0500

    WIP

--- studentmanagment/src/main/java/studentmanagement/studentmanagment/StudentmanagmentApplication.java
+++ studentmanagment/src/main/java/studentmanagement/studentmanagment/StudentmanagmentApplication.java
@@ -11,3 +11,4 @@ public class StudentmanagmentApplication {
 	}
 
 }
+// This is the main class of the application which is used to run the application. It is annotated with @SpringBootApplication which is a combination of @Configuration, @EnableAutoConfiguration and @ComponentScan annotations. It is used to enable auto-configuration and component scanning in the application. The main method is used to run the application.
